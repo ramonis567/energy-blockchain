a16e5d67c1ce0f3e17d1b4023ee55701f08815a5
ramonis567 <ramonmatosferreira@gmail.com>
2025-02-23 22:29:37 -0300

    Refatoção da estrutura dos dados de usuário e adição de script para geração de padrões de consumo, geração e armazenamento dos elementos da rede

---
+++ simulation/src/generators/init_user_data.py
@@ -0,0 +1,35 @@
+import random
+
+def get_average_energy_consumption(user_type, user_class):
+    if user_type == "consumer" or "prosumer":
+        if user_class == "residential":
+            return round(random.uniform(0.3, 2), 3)
+        elif user_class == "commercial":
+            return round(random.uniform(1, 10), 3)
+        elif user_class == "industrial":
+            return round(random.uniform(5, 30), 3)
+        else:
+            return 0
+    else:
+        return 0
+    
+def get_average_energy_generation(user_type, user_class):
+    if user_type == "prosumer" or user_type == "producer":
+        if user_class == "residential":
+            return round(random.uniform(0.1, 1.7), 3)
+        elif user_class == "commercial":
+            return round(random.uniform(0.5, 5), 3)
+        elif user_class == "industrial":
+            return round(random.uniform(5, 15), 3)
+        elif user_class == "energy_supplier":
+            return round(random.uniform(5, 15), 3)
+        else:
+            return 0
+    else:
+        return 0
+    
+def get_energy_storage_capacity(user_type):
+    if user_type == "battery":
+        return round(random.uniform(10, 50), 3)
+    else:
+        return 0
--- simulation/src/simulation.py
+++ simulation/src/simulation.py
@@ -2,32 +2,49 @@ import json
 import time
 import random     ## REMOVE THIS AFTER IMPLEMENTING THE SIMULATION
 
+from generators.init_user_data import get_average_energy_consumption, get_average_energy_generation, get_energy_storage_capacity
 from mqtt_client import publish_data
 from config import MQTT_UPDATE_INTERVAL
 
-users = json.load(open("simulation/src/data/users.json"))["users"]
+users_static = []
 
 def simulate():
+    global users_static
+
     while True:
-        users = json.load(open("simulation/src/data/users.json"))["users"]
-        
-        for user in users:
-            user_id= user.get("id")
-            user_name = user.get("name")
-            user_type = user.get("type")
-            topic = f"energy/{user_type}/{user_id}"
+        users_dynamic = json.load(open("simulation/src/data/users.json"))["users"]
 
-            payload = {
-                "user_id": user_id,
-                "user_name": user_name,
-                "user_type": user_type,
-                "energy_consumption": random.randint(1, 5),
-                "energy_generation": random.randint(1, 5)
-            }
+        ids_users_dynamic = {user["id"] for user in users_dynamic}
+        id_users_static = {user["id"] for user in users_static}
 
-            payload_json = json.dumps(payload)
-            publish_data(topic, payload_json)
+        removed_users = id_users_static - ids_users_dynamic
+        added_users = ids_users_dynamic - id_users_static
 
+        users_static = [user for user in users_static if user["id"] not in removed_users]
+
+        for user in users_dynamic:
+            if user["id"] in added_users:
+                user["average_energy_consumption"] = get_average_energy_consumption(user["type"], user["class"])
+                user["average_energy_generation"] = get_average_energy_generation(user["type"], user["class"])
+                user["energy_storage_capacity"] = get_energy_storage_capacity(user["type"])
+
+                users_static.append(user)
+
+        # for user in users_dynamic:
+        #     user_id= user.get("id")
+        #     user_type = user.get("type")
+        #     user_class = user.get("class")
+        #     topic = f"energy/{user_type}/{user_id}"
+
+        #     payload = {
+        #         "user_id": user_id,
+        #         "user_type": user_type,
+        #         "energy_consumption": random.randint(1, 5),
+        #         "energy_generation": random.randint(1, 5)
+        #     }
+
+        #     payload_json = json.dumps(payload)
+        #     publish_data(topic, payload_json)
 
         time.sleep(MQTT_UPDATE_INTERVAL)
 
