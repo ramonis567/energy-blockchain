6bc168c654c5c4e6922dcdfea0051813d476eeea
ramonis567 <ramonmatosferreira@gmail.com>
2025-03-07 16:52:00 -0300

    Iniciados arquivos da camada de interligação

--- simulation/src/generators/simulate_data.py
+++ simulation/src/generators/simulate_data.py
@@ -5,16 +5,16 @@ def simulate_user_consumption(user_type, user_class, user_avg_consumption):
     if user_type == "producer" or user_type == "battery":
         return 0
     else:
-        return np.random.normal(loc=user_avg_consumption, scale=0.4167)
+        return np.abs(np.random.normal(loc=user_avg_consumption, scale=0.4167))
 
 def simulate_user_generation(user_type, user_class, user_avg_generation):
     if user_type == "consumer" or user_type == "battery":
         return 0
     else:
-        return np.random.normal(loc=user_avg_generation, scale=0.4167)
+        return np.abs(np.random.normal(loc=user_avg_generation, scale=0.4167))
 
 def simulate_storage(user_type, user_class, energy_storage_cap):
     if user_type == "consumer" or user_type == "producer" or user_type == "prosumer":
         return 0
     else:
-        return np.random.normal(loc=energy_storage_cap, scale=0.4167)
+        return np.abs(np.random.normal(loc=energy_storage_cap, scale=0.4167))
